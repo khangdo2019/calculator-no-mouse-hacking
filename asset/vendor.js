3c23cad1d9560ce9de3d7da4b9b8801625aab1b5
Khang Do <khangdohuu.vccb@gmail.com>
2020-09-10 00:33:35 -0400

    set up site

---
+++ asset/vendor.js
@@ -0,0 +1,13 @@
+const userInput = document.getElementById('input-number');
+const kilogramBtn = document.getElementById('btn-kilogram');
+const gramBtn = document.getElementById('btn-gram');
+const ounceBtn = document.getElementById('btn-ounce');
+const clearBtn = document.getElementById('btn-clear');
+
+const currentResultOutput = document.getElementById('current-result');
+const currentCalculationOutput = document.getElementById('current-calculation');
+
+const outputResult = (result, text) {
+    currentResultOutput.textContent = result;
+    currentCalculationOutput.textContent = text;
+};
